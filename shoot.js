77c4bc1303946a74300664d1945fca0363f889e3
Jeff <jmschwarting@gmail.com>
2015-05-18 17:31:04 -0600

    shells fire from tank position now

--- shoot.js
+++ shoot.js
@@ -3,7 +3,7 @@ var shellCount = 0;
 function shoot () {
 	var shellID = "shell" + shellCount;
 	var cleanID = "#" + shellID;
-	$("#field").append('<div class="shell" id='+shellID+'"></div>');
+	$("#field").append('<div class="shell" style="top: '+tank1Position+'px" id='+shellID+'"></div>');
 	$(".shell").animate({right: "-32px"}, "slow");
 	shellCount ++;
 };

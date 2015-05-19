ae353fe36914462d234c3e571931ca31a465f722
Jeff <jmschwarting@gmail.com>
2015-05-18 18:29:35 -0600

    pushing stufffffff

--- shoot.js
+++ shoot.js
@@ -1,11 +1,18 @@
 var shellCount = 0;
 
-function shoot () {
-	var shellID = "shell" + shellCount;
-	var cleanID = "#" + shellID;
-	$("#field").append('<div class="shell" style="top: ' + tank1Position + 'px" id=' + shellID + '"></div>');
-	$(".shell").animate({right: "-50px"}, shellSpeed, 'linear');
-	shellCount ++;
+var shooting = true;
 
-	detectGameOver(tank1Position);
-};
+	function shoot () {
+		if (shooting) {
+			var shellID = "shell" + shellCount;
+			var cleanID = "#" + shellID;
+			$("#field").append('<div class="shell" style="top: ' + tank1Position + 'px" id=' + shellID + '"></div>');
+			$(".shell").animate({right: "-50px"}, shellSpeed, 'linear');
+			shellCount ++;
+			shooting = false;
+			setTimeout(function(){
+				shooting=true}, 500);
+
+			detectGameOver(tank1Position);
+		};	
+	};

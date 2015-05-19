2431bec38bfdb41cc7a4ddab7cd1c9be54dcf4d8
Jeff <jmschwarting@gmail.com>
2015-05-19 14:43:00 -0600

    adding sound

--- shoot.js
+++ shoot.js
@@ -3,8 +3,16 @@ var shooting = true;
 var shootDelay = 500;
 var autoShootingSpeed = 500;
 
+var fireAudio = new Audio('fire.wav');
+var fireAudio2 = new Audio('fire2.wav');
+
+
 function shoot () {
 	if (shooting) {
+		fireAudio.pause();
+		fireAudio.currentTime = 0;
+		fireAudio.play();
+
 		var shellID = "shell" + shellCount;
 		var cleanID = "#" + shellID;
 		$("#field").append('<div class="shell" style="top: ' + tank1Position + 'px" id=' + shellID + '"></div>');
@@ -30,6 +38,10 @@ var autoShooting;
 var getShooting = function () {
 	autoShooting = setInterval (
 		function shoot2 () {
+			fireAudio2.pause();
+			fireAudio2.currentTime = 0;
+			fireAudio2.play();
+
 			var shell2ID = "shell" + shellCount2;
 			var cleanID = "#" + shell2ID;
 			$("#field").append('<div class="shell2" style="top: ' + tank2Position + 'px; left: 650px" id=' + shell2ID + '"></div>');

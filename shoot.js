61365cfec55323e09928e535752b9f57907f12f7
Jeff <jmschwarting@gmail.com>
2015-05-18 19:10:26 -0600

    dslfkj

--- shoot.js
+++ shoot.js
@@ -1,24 +1,17 @@
 var shellCount = 0;
-
 var shooting = true;
 
-function shoot () {
-	if (shooting) {
-		var shellID = "shell" + shellCount;
-		var cleanID = "#" + shellID;
+	function shoot () {
+		if (shooting) {
+			var shellID = "shell" + shellCount;
+			var cleanID = "#" + shellID;
+			$("#field").append('<div class="shell" style="top: ' + tank1Position + 'px" id=' + shellID + '"></div>');
+			$(".shell").animate({right: "-50px"}, shellSpeed, 'linear');
+			shellCount ++;
+			shooting = false;
+			setTimeout(function(){
+				shooting=true}, 500);
 
-		// send missile
-		$("#field").append('<div class="shell" style="top: ' + tank1Position + 'px" id=' + shellID + '"></div>');
-		$(".shell").animate({right: "-50px"}, shellSpeed, 'linear');
-		shellCount ++;
-
-		// preventing fast shooting
-		shooting = false;
-		setTimeout( function () {
-			shooting = true
-		}, 500);
-
-		// check game over
-		detectGameOver(tank1Position);
-	};	
-};
+			detectGameOver(tank1Position);
+		};	
+	};

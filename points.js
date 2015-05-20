be0fca00197abc8c4b4a49ba536537303e6b2b51
Daniel Falabella <dnlfala@gmail.com>
2015-05-20 14:27:28 -0600

    Loading font through https

--- points.js
+++ points.js
@@ -2,7 +2,7 @@ var firebase = new Firebase("https://tankgame.firebaseio.com/");
 
 var getHighScore = function () {
 	firebase.on('value', function (data) {
-		allData = data.val()
+		allData = data.val();
 		highestScore = allData.score;
 		$('#high-score').html(highestScore);
 		$('#high-player').html(allData.player);
